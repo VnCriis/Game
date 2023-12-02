ff54f2aeab86c77dc4fac88c19d399f0790cfee0
Cristian Usiña <cusinac@gmail.com>
2023-12-01 19:39:37 -0500

    Seleccion de palabras

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 public class Main{
     public static void main (String[] args){
+        String[] palabras = {"encebollado","fritada","guatita"};
+        String palabra = palabras[(int)(Math.random() * palabras.length)];
+        char[] palabraAdivinada = new char[palabra.length()];
+        boolean juegoTerminado = false;
+        int intentos = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < palabraAdivinada.length; i++) {
+            palabraAdivinada[i] = '_';
+        }
 
     }
 }
