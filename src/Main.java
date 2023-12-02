1803681196e5902f5dacab83777e41d765d38314
Cristian Usiña <cusinac@gmail.com>
2023-12-01 19:43:17 -0500

    Ingreso de los intentos

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,19 @@ public class Main{
         for (int i = 0; i < palabraAdivinada.length; i++) {
             palabraAdivinada[i] = '_';
         }
+        while (!juegoTerminado) {
+            System.out.println("Palabra: " + String.valueOf(palabraAdivinada));
+            System.out.println("Intentos restantes: " + (6 - intentos));
+            System.out.print("Ingresa una letra: ");
+            char letra = scanner.next().charAt(0);
 
+            boolean letraAdivinada = false;
+            for (int i = 0; i < palabra.length(); i++) {
+                if (palabra.charAt(i) == letra) {
+                    palabraAdivinada[i] = letra;
+                    letraAdivinada = true;
+                }
+            }
+        }
     }
 }
